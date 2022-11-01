97d512edcb079e53dd3c13c41b18dc991437be0a
DESKTOP-B2ESKHB\olhat <olhatytysh@gmail.com>
2022-11-01 11:07:25 +0200

    first commit

---
+++ UnitTest5.1/UnitTest5.1.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest51
+{
+	TEST_CLASS(UnitTest51)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double x, eps, int& n, & s;
+			result C
+		}
+	};
+}
